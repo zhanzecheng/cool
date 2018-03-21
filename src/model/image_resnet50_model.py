933ce66f29763fdbcc5e4a1d62cb8b25d84cddff
yph <yu_pei_hua@hotmail.com>
2018-03-21 13:26:56 +0800

    debug

--- src/model/image_resnet50_model.py
+++ src/model/image_resnet50_model.py
@@ -1,13 +1,15 @@
 from keras.models import Model
 from keras.layers import Input, Dense, Flatten
 from keras.layers import Dropout
+from keras.optimizers import Adam
+from keras.applications.vgg16 import VGG16
 from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.preprocessing import image
 import pandas as pd
 import numpy as np
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 image_dir = "/home/zzc/cool/data/sohu/"
 image_input_shape = (224, 224, 3)
@@ -17,10 +19,21 @@ image_label_file = "../../data/image_label.csv"
 
 image_label_df = pd.read_csv(image_label_file)
 
+# image_label_df = image_label_df.head(1000)
 
-def batch_iter(dir, train, batch_size, epoch, shuffle=True):
+
+def imagenet_scale(x):
+    """ImageNet is trained with the following mean pixels.
+    """
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
+    return x
+
+
+def batch_iter(dir, train, batch_size, epochs, shuffle=True):
     """ Generates a batch iterator"""
-    for epoch in range(epoch):
+    for epochs in range(epochs):
         data_size = len(train['image_name'])
         if shuffle:
             shuffle_indices = np.random.permutation(np.arange(data_size))
@@ -38,36 +51,44 @@ def batch_iter(dir, train, batch_size, epoch, shuffle=True):
 
             for name in shuffle_image_name[train_start:train_end]:
                 path = ''
+
                 for i in range(1, 4):
                     tmp_path = dir + 'Pic_info_train.part' + str(i) + '/' + name
                     if os.path.exists(tmp_path):
                         path = tmp_path
                         break
+                try:
+                    img = image.load_img(path, target_size=(224, 224))
+                except OSError:
+                    img = np.zeros(shape=(224, 224, 3))
 
-                img = image.load_img(path, target_size=(224, 224))
                 x = image.img_to_array(img)
+                x = imagenet_scale(x)
                 x = np.expand_dims(x, axis=0)
                 x = preprocess_input(x)
                 train_image.extend(x)
 
-            yield np.array(train_image), shuffle_image_label[train_start:train_end]
+            yield (np.array(train_image), shuffle_image_label[train_start:train_end])
 
 
 def get_model():
     input_image = Input(shape=(224, 224, 3), name='image_input')
+    # base_model = VGG16(input_tensor=input_image, weights='imagenet', include_top=False)
     base_model = ResNet50(input_tensor=input_image, weights='imagenet', include_top=False)
     dense_image1 = Dense(256, activation='relu', name='dense1')(base_model.output)
     dropout_image1 = Dropout(0.5, name='dropout1')(dense_image1)
     flatten = Flatten()(dropout_image1)
     predictions = Dense(1, activation='sigmoid', name='dense2')(flatten)
     res_model = Model(inputs=[input_image], outputs=[predictions])
-    res_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    optimizer = Adam(0.001, 0.9, 0.999, None, 0.00001)
+    res_model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     return res_model
 
 
 model = get_model()
-model.fit_generator(batch_iter(image_dir, image_label_df, 32, 10), steps_per_epoch=11067)
-model.save_weights(filepath='weights')
+steps_per_epoch = int((len(image_label_df) - 1) / image_batch_size) + 1
+model.fit_generator(batch_iter(image_dir, image_label_df, image_batch_size, epochs=10), steps_per_epoch=steps_per_epoch, epochs=10)
+model.save_weights(filepath='/home/yph/weights')
 
 
 
